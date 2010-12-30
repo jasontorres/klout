47d38d5467473aafd22c6afee180787eca54363d
Jason Goecke <jason@goecke.net>
2010-12-30 08:57:39 -0800

    Added the appropriate dependencies.

--- lib/klout.rb
+++ lib/klout.rb
@@ -1,5 +1,8 @@
 require 'rubygems'
 require 'json'
+require 'open-uri'
+require 'net/http'
+
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
